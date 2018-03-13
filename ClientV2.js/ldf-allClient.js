8a47669a389448af0343b7e188924e0d82e8b0c4
HacquesF <florian.hacques@etu.univ-nantes.fr>
2018-03-13 12:31:16 +0100

    With the bar?

---
+++ ClientV2.js/ldf-allClient.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var ldf = require('ldf-client');
+ldf.Logger.setLevel('warning');
+
+//Set up args
+var argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+const fileName = argv._.pop();
+const server = argv._;
+
+//If no args, just the first one
+const nbQueries = argv.e || 1;
+const startQuerie = argv.s || 0;
+//Read queries file
+//const server = process.argv[2]
+var file = fs.readFileSync(fileName, 'utf8');
+var queries = [];
+queries = JSON.parse(file);
+
+
+queries = queries.slice(startQuerie, nbQueries)
+function execute(query) {
+   return new Promise(resolve => {
+      //var results = new ldf.SparqlIterator(queries[value], { fragmentsClient: fragmentsClient })
+	var fragmentsClient = new ldf.FragmentsClient(server);
+	var result = new ldf.SparqlIterator(query, { fragmentsClient: fragmentsClient })
+	result.on('data', (res) => {
+		//
+	})
+	result.on('end', () => {
+	   //console.log('done')
+		resolve()
+	})
+   })
+}
+
+process.stdout.write("[");
+queries.reduce((acc, query) => acc.then((globalResult) => {
+//	return new Promise((resolve, reject) => {
+//		return execute(query).then((res) => {
+//			return Promise.resolve([...globalResult, res])
+//		})
+//	})
+   process.stdout.write("#");
+	return execute(query)
+}), Promise.resolve([])).then((finalResult) => {
+   process.stdout.write("]\n");
+	console.log('Finished', finalResult)
+	
+})
+
+
+
+//      var results = new ldf.SparqlIterator(queries[value], { fragmentsClient: fragmentsClient });
+
+
