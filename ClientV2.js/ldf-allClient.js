e034a41a1e7004beae3d2e75f648329835f9c218
HacquesF <florian.hacques@etu.univ-nantes.fr>
2018-03-19 22:02:15 +0100

    Starting client cache experiment

--- ClientV2.js/ldf-allClient.js
+++ ClientV2.js/ldf-allClient.js
@@ -1,13 +1,19 @@
 var fs = require('fs');
 var ldf = require('ldf-client');
-ldf.Logger.setLevel('warning');
+var Cache = require('lru-cache');
 
+//e = id of last queries
+//s = id of first queries
+//l = type of log
+//c = max cache size
 //Set up args
 var argv = require('minimist')(process.argv.slice(2));
 console.dir(argv);
 const fileName = argv._.pop();
 const server = argv._;
 
+//Set up log level
+   ldf.Logger.setLevel(argv.l || 'warning');
 //If no args, just the first one
 const nbQueries = argv.e || 1;
 const startQuerie = argv.s || 0;
@@ -23,6 +29,7 @@ function execute(query) {
    return new Promise(resolve => {
       //var results = new ldf.SparqlIterator(queries[value], { fragmentsClient: fragmentsClient })
 	var fragmentsClient = new ldf.FragmentsClient(server);
+	fragmentsClient._cache = new Cache({ max: argv.c || 100 });
 	var result = new ldf.SparqlIterator(query, { fragmentsClient: fragmentsClient })
 	result.on('data', (res) => {
 		//
@@ -45,7 +52,7 @@ queries.reduce((acc, query) => acc.then((globalResult) => {
 	return execute(query)
 }), Promise.resolve([])).then((finalResult) => {
 //   process.stdout.write("]\n");
-	console.log('Finished', finalResult)
+	//console.log('Finished', finalResult)
 	
 })
 
